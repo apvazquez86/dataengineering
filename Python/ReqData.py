c2aae56c1c6688a9fbd4117bcb5a0a852e013d3e
Arcel Perdomo <aperdomo@arnaldocastro.com.uy>
2021-11-24 11:01:51 -0300

    Uploading Python code

---
+++ Python/ReqData.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import configparser
+from datetime import date,timedelta,datetime
+
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+
+
+payload={}
+headers = {}
+
+url = config['URL']['urlAddress']
+countries = [e.strip() for e in config.get('Countries', 'countryList').split(',')]
+
+dataInsert = []
+
+yesterday = date.today() - timedelta(days=1)
+yesterday.strftime('%Y-%m-%d')
+
+
+sDate = str(yesterday)
+eDate = str(yesterday)
+
+urlDynamic = url.replace("TAG_SDATE",sDate)
+urlDynamic = urlDynamic.replace("TAG_EDATE",eDate)
+
+
+for idx,c in enumerate(countries):
+    
+    if "TAG_COUNTRY" in urlDynamic:
+        urlDynamic = urlDynamic.replace("TAG_COUNTRY", str(c))
+    else:
+        previousCountry = countries[idx-1]
+        urlDynamic = urlDynamic.replace(previousCountry, str(c))
+    
+    # Getting the response 
+    response = requests.request("GET", urlDynamic, headers=headers, data=payload)
+
+    # Converting Response Text to a Dictionary
+    response_dict = json.loads(response.text)
+
+    # Printing only a few values
+    row = {'Pais': response_dict[0]["country"], 'Casos': response_dict[0]["confirmed_daily"], 'Recuperados': response_dict[0]["recovered_daily"], 'Fallecidos': response_dict[0]["deaths_daily"] }
+    dataInsert.append(row)
+
+print(dataInsert)
